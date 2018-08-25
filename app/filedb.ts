8abbb356d7c8588e3b3d399beac0291a1d5d2738
Carsten Führmann <cfuhrmann@gmail.com>
2018-08-25 22:18:29 +0200

    Fixed some typing issues in filedb

--- app/filedb.ts
+++ app/filedb.ts
@@ -1,5 +1,5 @@
-import { Test, DataBase } from './types';
-import * as fs from 'fs';
+import { Test, DataBase } from "./types";
+import * as fs from "fs";
 
 export const createFileDb = (fileName: string) => {
   const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
@@ -11,26 +11,26 @@ export const createFileDb = (fileName: string) => {
     const data = JSON.parse(json, reviver) as Test[]; // todo: runtime check if the type assertion is correct?
 
     return {
-      createTest(test: Test) {
-        const [result] = data.filter((t: Test) => t.id === test.id);
-        if (result) throw new Error('Key already exists!');
+      createTest(test) {
+        const [result] = data.filter(t => t.id === test.id);
+        if (result) throw new Error("Key already exists!");
         data.push({ ...test });
         writeJsonToFile(data);
       },
 
-      getTest(id: string) {
-        const [result] = data.filter((test: Test) => test.id === id);
+      getTest(id) {
+        const [result] = data.filter(test => test.id === id);
         if (result !== undefined) return { ...result };
       },
 
-      findTests(substring: string) {
+      findTests(substring) {
         return data
           .filter(
-            (test: Test) =>
+            test =>
               test.prompt.includes(substring) ||
               test.solution.includes(substring)
           )
-          .map((test: Test) => ({ ...test }));
+          .map(test => ({ ...test }));
       },
 
       updateTest({
@@ -40,46 +40,32 @@ export const createFileDb = (fileName: string) => {
         state,
         changeTime,
         lastTicks,
-        nextTime,
-      }: Test) {
+        nextTime
+      }) {
         for (const test of data) {
           if (test.id !== id) continue;
 
-          if (prompt !== undefined)
-            if (typeof prompt === 'string') test.prompt = prompt;
-            else throw new TypeError("'prompt' is not string!");
-
-          if (solution !== undefined)
-            if (typeof solution === 'string') test.solution = solution;
-            else throw new TypeError("'solution' is not string!");
-
+          if (prompt !== undefined) test.prompt = prompt;
+          if (solution !== undefined) test.solution = solution;
           if (state !== undefined) test.state = state;
-
-          if (changeTime !== undefined)
-            if (changeTime instanceof Date) test.changeTime = changeTime;
-            else throw new TypeError("'changeTime' is not a Date!");
-
-          if (lastTicks !== undefined)
-            if (typeof lastTicks === 'number') test.lastTicks = lastTicks;
-            else throw new TypeError("'lastTicks' is not a number!");
-
-          if (nextTime !== undefined)
-            if (nextTime instanceof Date) test.nextTime = nextTime;
-            else throw new TypeError("'nextTime' is not a Date!");
+          if (changeTime !== undefined) test.changeTime = changeTime;
+          if (lastTicks !== undefined) test.lastTicks = lastTicks;
+          if (nextTime !== undefined) test.nextTime = nextTime;
 
           writeJsonToFile(data);
+
           return { ...test };
         }
       },
 
-      findNextTest(time: Date) {
+      findNextTest(time) {
         return data
-          .filter((test: Test) => test.nextTime <= time)
+          .filter(test => test.nextTime <= time)
           .sort(
-            (test1: Test, test2: Test) =>
+            (test1, test2) =>
               test1.nextTime.getTime() - test2.nextTime.getTime()
           )[0];
-      },
+      }
     };
   }
 
@@ -96,6 +82,6 @@ export const createFileDb = (fileName: string) => {
   }
 
   function isDateString(value: string) {
-    return typeof value === 'string' && dateFormat.test(value);
+    return typeof value === "string" && dateFormat.test(value);
   }
 };
