932099bb085149c44982d9609ae79dc7985ecfef
cfuehrmann <carsten.fuehrman@allegion.com>
2019-06-17 17:50:39 +0200

    Moved the database file up in the folder hierarchy

--- app/production-config.ts
+++ app/production-config.ts
@@ -2,5 +2,5 @@ import { createFileRepositoryTools } from "./file-repository-tools";
 
 // Configuration of the repository to avoid the "constrained construction" antipattern
 export const repositoryTools = createFileRepositoryTools(
-  __dirname + "/production-file-repository.json",
+  __dirname + "/../production-file-repository.json",
 );
